58d2238117848952a4d1c265987f822c52373e84
layvimol <lay.chivoanvimol19@kit.edu.kh>
2023-08-11 23:19:13 +0700

    Finished sign in and started page and pull tabwrappers

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,7 +1,48 @@
 import 'package:flutter/material.dart';
 
-class HomeView extends StatelessWidget {
-  const HomeView({super.key});
+class HomeView extends StatefulWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  late PageController _pageController;
+  int _currentPage = 0;
+  final int _numPages = 3; // Number of pages (images) in your PageView
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: _currentPage);
+    _startAutoScroll();
+  }
+
+  void _startAutoScroll() {
+    Future.delayed(const Duration(seconds: 3), () {
+      if (_currentPage < _numPages - 1) {
+        _pageController.nextPage(
+          duration: const Duration(milliseconds: 500),
+          curve: Curves.easeInOut,
+        );
+        _currentPage++;
+        _startAutoScroll(); // Call again for the next auto-scroll
+      } else {
+        // Reset to the first page and restart auto-scroll
+        _pageController.jumpToPage(0);
+        _currentPage = 0;
+        _startAutoScroll();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
