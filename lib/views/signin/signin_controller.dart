00c9f5af232a550cbde626be80adc191030aeae4
khengpitou <kheng.pitou19@kit.edu.kh>
2023-09-14 02:07:15 +0700

    Details_screens

--- lib/views/signin/signin_controller.dart
+++ lib/views/signin/signin_controller.dart
@@ -8,10 +8,10 @@ import 'package:diary_journal/core/api/utils/save_local_data.dart';
 import 'package:diary_journal/core/routes/app_routes.dart';
 import 'package:diary_journal/theme/theme_color.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_web_auth/flutter_web_auth.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:http/http.dart' as http;
-import 'package:flutter_web_auth/flutter_web_auth.dart';
 
 class SignInController extends GetxController {
   final TextEditingController emailOrUsernameController =
